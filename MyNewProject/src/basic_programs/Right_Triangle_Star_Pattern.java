597c670c1da32d66817d2c5f4edcd17fac6d0ce7
ankushhage <ankushhage2192@gmail.com>
2022-12-27 10:41:41 +0530

    Right_Triangle_Star_Pattern

---
+++ MyNewProject/src/basic_programs/Right_Triangle_Star_Pattern.java
@@ -0,0 +1,28 @@
+package basic_programs;
+
+
+//Java code to demonstrate right star triangle
+public class Right_Triangle_Star_Pattern 
+{
+	// Function to demonstrate printing pattern
+	public static void StarRightTriangle(int n)
+	{
+		int a,b;
+		for(a = 0; a < n; a++)
+		{
+			for(b = 0; b <= a; b++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	
+	}
+
+	// Driver Function
+	public static void main(String args[])
+	{
+		int k = 5;
+		StarRightTriangle(k);
+	}
+}
